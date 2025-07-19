7e95d1ea530965f3d4f6f7fb13f0bfa9012251e4
Shivani Sharma <shivanisharma@Shivanis-MacBook-Air.local>
2025-07-19 14:59:46 +0530

    added logo and other modifications done

--- src/components/TopNavigation.tsx
+++ src/components/TopNavigation.tsx
@@ -2,7 +2,7 @@ import '../styles/TopNavigation.css'
 import SearchImg from '../assets/v6-icon (free).png'
 import AccountImg from '../assets/v6-icon (free) (1).png'
 import ShoppingImg from '../assets/v6-icon (free) (2).png'
-import CoralImg from '../assets/Group 98.png'
+import Logo from '../assets/logo.png'
 
 export const TopNavigation = () => {
     return (
@@ -11,7 +11,7 @@ export const TopNavigation = () => {
             <img src={SearchImg} />
         </div>
         <div className="logo-container">
-            <img src={CoralImg}></img>CORAL<img src={CoralImg}></img>
+            <img src={Logo}></img>
         </div>
         <div className="right-container">
             <div className="account-container">
